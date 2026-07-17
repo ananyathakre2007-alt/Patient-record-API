21af190bac5ab766e0953a438fb93a624d8041b6
Ananya Thakre <ananyathakre2007@gmail.com>
2026-07-17 12:19:11 +0530

    Path Parameter and Query Parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path, HTTPException,Query
 import json
 
 app=FastAPI()
@@ -28,7 +28,7 @@ def view():
 
 #dynamic segment
 @app.get("/patient/{patient_id}")
-def view_patient(patient_id : str):
+def view_patient(patient_id : str=Path(...,description='id of the patient in DB',example='P001')):
      
     #load all patients
     data=load_data()
@@ -36,7 +36,24 @@ def view_patient(patient_id : str):
     if patient_id in data:
         return data[patient_id]
     
-    else:
-        return {"Error":"Patient not found"}
     
+    raise HTTPException(status_code=404,detail='Patient not found')
 
+@app.get('/sort')
+def sort_patients(sort_by : str= Query(...,description='sort patients on the basis of height,weight or bmi'),order:str=Query('asc',description='Arrange in asc or desc order')):
+    valid_fields=['height','weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f'Invalid request... select from {valid_fields}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException (status_code=400,detail='Invalid request ... select asc or desc')
+
+    data=load_data()
+    sort_order=True if order=='desc' else False
+
+    sorted_data=sorted(data.values(), key=lambda x: x.get(sort_by, 0),reversed=sort_order)
+
+    return sorted_data
+
+    
